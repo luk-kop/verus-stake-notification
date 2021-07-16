fd944a4329fa4d7cdb9b1415839b16eb2e8dc25e
luk-kop <luk.kopicki@gmail.com>
2021-07-16 11:45:30 +0200

    Updated aws_cognito.py file (added CognitoUserPoolClient class).
    Details:
    - Added scope_identifiers attribute to CognitoResourceServer class.
    - Added domain attribute to CognitoUserPool class.

--- resources/aws_cognito.py
+++ resources/aws_cognito.py
@@ -26,7 +26,7 @@ class CognitoUserPool:
             )
             self.arn = cognito_user_pool['UserPool']['Arn']
             self.id = cognito_user_pool['UserPool']['Id']
-            print(f'The Cognito "{self.name}" user pool created')
+            print(f'The Cognito "{self.name}" user pool created.')
             return
         print(f'The Cognito "{self.name}" user pool exists. Using it.')
 
@@ -48,11 +48,22 @@ class CognitoUserPool:
         """
         return self._cognito_client.list_user_pools(MaxResults=60)['UserPools']
 
-    def get_arn(self):
+    def get_arn(self) -> str:
         """
         Returns Cognito user pool ARN.
         """
         return self._cognito_client.describe_user_pool(UserPoolId=self.id)['UserPool']['Arn']
+
+    @property
+    def domain(self) -> str:
+        """
+        Domain for Cognito user pool.
+        Domain will be assigned to user pool resource with CognitoUserPoolDomain class instance.
+        """
+        try:
+            return self._cognito_client.describe_user_pool(UserPoolId=self.id)['UserPool']['Domain']
+        except KeyError:
+            return ''
 
     def delete_resource(self):
         """
@@ -82,6 +93,13 @@ class CognitoResourceServer:
             'ScopeDescription': description
         }
         self._scopes.append(scope)
+
+    @property
+    def scope_identifiers(self) -> list:
+        """
+        Returns list of scope identifiers in format 'identifier/scope_name'.
+        """
+        return [f'{self.id}/{scope["ScopeName"]}'for scope in self._scopes]
 
     def create_resource(self) -> None:
         """
@@ -161,19 +179,100 @@ class CognitoUserPoolDomain:
             print(err.response['Error']['Message'])
 
 
+class CognitoUserPoolClient:
+    """
+    Class represents domain for Cognito user pool client.
+    """
+    def __init__(self, name: str, user_pool_id: str, scopes: list) -> None:
+        self.name = name
+        self.user_pool_id = user_pool_id
+        self.scopes = scopes
+        self._cognito_client = boto3.client('cognito-idp')
+        self.id = self._get_client_id()
+
+    def create_resource(self) -> None:
+        """
+        Creates Cognito user pool client resource in AWS cloud.
+        """
+        if not self._check_exist():
+            resource = self._cognito_client.create_user_pool_client(
+                ClientName=self.name,
+                UserPoolId=self.user_pool_id,
+                GenerateSecret=True,
+                AllowedOAuthFlows=['client_credentials'],
+                AllowedOAuthScopes=self.scopes
+            )
+            print(f'The Cognito user pool client "{self.name}" created')
+            self.id = resource['UserPoolClient']['ClientId']
+            return
+        print(f'The Cognito user pool client "{self.name}" exists. Using it.')
+
+    @property
+    def _user_pool_clients(self) -> list:
+        """
+        Returns list of already created Cognito user pool clients.
+        """
+        return self._cognito_client.list_user_pool_clients(MaxResults=60,
+                                                           UserPoolId=self.user_pool_id)['UserPoolClients']
+
+    def _get_client_id(self) -> str:
+        """
+        Returns user pool client id.
+        """
+        for client in self._user_pool_clients:
+            if client['ClientName'] == self.name:
+                return client['ClientId']
+        return ''
+
+    def _remove_client_id(self) -> None:
+        """
+        Clear 'id' attribute.
+        """
+        self.id = ''
+
+    def _check_exist(self) -> bool:
+        """
+        Checks if Cognito user pool client resource with specified name already exist.
+        Assign 'id' attribute if user pool client exist.
+        """
+        result = True if self.id else False
+        return result
+
+    def delete_resource(self) -> None:
+        """
+        Deletes Cognito user pool client in AWS cloud.
+        """
+        if self._check_exist():
+            self._cognito_client.delete_user_pool_client(UserPoolId=self.user_pool_id,
+                                                         ClientId=self.id)
+            print(f'The Cognito user pool client "{self.name}" has been deleted')
+            self._remove_client_id()
+            return
+        print(f'The Cognito user pool client "{self.name}" does not exist')
+
+
 if __name__ == '__main__':
+    # Create Cognito User Pool
     pool = CognitoUserPool(name='TestPool')
     pool.create_resource()
+    # Create Resource Server
     resource_srv = CognitoResourceServer(name='verus-api-resource-server',
                                          identifier='verus-api',
                                          user_pool_id=pool.id)
     resource_srv.add_scope(name='api-read',
                            description='Read access to the API')
     resource_srv.create_resource()
-
+    # Create User Pool domain
     domain = CognitoUserPoolDomain(domain_prefix='verus-test123', user_pool_id=pool.id)
     domain.create_resource()
-
+    # Create User Pool Client
+    user_pool_client = CognitoUserPoolClient(name='verus-cli-wallet',
+                                             user_pool_id=pool.id,
+                                             scopes=resource_srv.scope_identifiers)
+    user_pool_client.create_resource()
+    # Delete all Cognito related resources.
+    user_pool_client.delete_resource()
     domain.delete_resource()
     resource_srv.delete_resource()
+    user_pool_client.delete_resource()
     pool.delete_resource()
