1d7c673be29ffdc2b2685d615bee14703385b969
luk-kop <luk.kopicki@gmail.com>
2021-09-14 19:39:21 +0200

    Updated terraform config (Lambda & API Gateway without auth).

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -74,8 +74,9 @@ resource "aws_lambda_function" "verus_lambda" {
   runtime          = "python3.8"
   environment {
     variables = {
-      TOPIC_ARN     = aws_sns_topic.verus_topic.arn
-      DYNAMODB_NAME = aws_dynamodb_table.verus_stakes_txids_table.id
+      TOPIC_ARN            = aws_sns_topic.verus_topic.arn
+      DYNAMODB_TXIDS_NAME  = aws_dynamodb_table.verus_stakes_txids_table.id
+      DYNAMODB_VALUES_NAME = aws_dynamodb_table.verus_stakes_values_table.id
     }
   }
   tags = var.resource_tags
@@ -86,7 +87,8 @@ resource "aws_lambda_permission" "verus_api_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.verus_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current_aws_account.account_id}:${aws_api_gateway_rest_api.verus_api.id}/*/${aws_api_gateway_method.verus_api.http_method}${aws_api_gateway_resource.verus_api.path}"
+  source_arn    = "${aws_api_gateway_rest_api.verus_api.execution_arn}/*/*/*"
+  //  source_arn    = "${aws_api_gateway_rest_api.verus_api.execution_arn}/*/${aws_api_gateway_method.verus_api_get.http_method}${aws_api_gateway_resource.verus_api.path}"
 }
 
 # Cognito config
@@ -124,7 +126,7 @@ resource "aws_cognito_user_pool_client" "verus_cognito_client" {
 # API Gateway config
 resource "aws_api_gateway_rest_api" "verus_api" {
   name        = "verus-api-gateway-${random_id.name.hex}"
-  description = "Invoke Lambda function to publish a msg to SNS topic when new stake appears in Verus wallet."
+  description = "Invoke Lambda function when a new stake appears in your Verus (VRSC) wallet."
   tags        = var.resource_tags
 }
 
@@ -135,46 +137,145 @@ resource "aws_api_gateway_resource" "verus_api" {
 }
 
 resource "aws_api_gateway_authorizer" "verus_auth" {
-  name          = "VerusApiAuth"
+  name          = "Verus-API-Authorizer"
   type          = "COGNITO_USER_POOLS"
   rest_api_id   = aws_api_gateway_rest_api.verus_api.id
   provider_arns = [aws_cognito_user_pool.verus_cognito_pool.arn]
 }
 
-resource "aws_api_gateway_method" "verus_api" {
-  authorization        = "COGNITO_USER_POOLS"
-  authorizer_id        = aws_api_gateway_authorizer.verus_auth.id
-  http_method          = "GET"
-  resource_id          = aws_api_gateway_resource.verus_api.id
-  rest_api_id          = aws_api_gateway_rest_api.verus_api.id
-  authorization_scopes = aws_cognito_resource_server.verus_cognito_resource_server.scope_identifiers
+# API Gateway - GET
+resource "aws_api_gateway_method" "verus_api_get" {
+  //  authorization        = "COGNITO_USER_POOLS"
+  authorization = "NONE"
+  //  authorizer_id        = aws_api_gateway_authorizer.verus_auth.id
+  http_method = "GET"
+  resource_id = aws_api_gateway_resource.verus_api.id
+  rest_api_id = aws_api_gateway_rest_api.verus_api.id
+  //  authorization_scopes = aws_cognito_resource_server.verus_cognito_resource_server.scope_identifiers
 }
 
-resource "aws_api_gateway_integration" "verus_api" {
-  http_method             = aws_api_gateway_method.verus_api.http_method
+resource "aws_api_gateway_integration" "verus_api_get" {
+  http_method             = aws_api_gateway_method.verus_api_get.http_method
   resource_id             = aws_api_gateway_resource.verus_api.id
   rest_api_id             = aws_api_gateway_rest_api.verus_api.id
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = aws_lambda_function.verus_lambda.invoke_arn
   connection_type         = "INTERNET"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  request_templates = {
+    "application/json" = <<EOF
+{
+    "year": "$input.params('year')",
+    "month": "$input.params('month')",
+    "http_method": "$context.httpMethod"
+}
+EOF
+  }
 }
 
-resource "aws_api_gateway_method_response" "verus_api_response_200" {
-  http_method = aws_api_gateway_method.verus_api.http_method
+resource "aws_api_gateway_method_response" "verus_api_method_response_get_200" {
+  http_method = aws_api_gateway_method.verus_api_get.http_method
   resource_id = aws_api_gateway_resource.verus_api.id
   rest_api_id = aws_api_gateway_rest_api.verus_api.id
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "response_200" {
-  http_method       = aws_api_gateway_method.verus_api.http_method
+resource "aws_api_gateway_integration_response" "verus_api_integration_response_get_200" {
+  http_method       = aws_api_gateway_method.verus_api_get.http_method
   resource_id       = aws_api_gateway_resource.verus_api.id
   rest_api_id       = aws_api_gateway_rest_api.verus_api.id
-  status_code       = aws_api_gateway_method_response.verus_api_response_200.status_code
+  status_code       = aws_api_gateway_method_response.verus_api_method_response_get_200.status_code
   selection_pattern = ""
   content_handling  = "CONVERT_TO_TEXT"
-  depends_on        = [aws_api_gateway_integration.verus_api]
+  depends_on        = [aws_api_gateway_integration.verus_api_get]
+}
+
+# API Gateway - POST
+resource "aws_api_gateway_model" "verus_api_post_model" {
+  rest_api_id  = aws_api_gateway_rest_api.verus_api.id
+  name         = "StakePOST"
+  description  = "JSON schema for stake POST method"
+  content_type = "application/json"
+  schema = <<EOF
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title" : "New Stake",
+  "type" : "object",
+  "properties": {
+      "txid": {
+          "description": "Stake transaction (tx) id",
+          "type": "string"
+      },
+      "time": {
+          "description": "Time of stake tx",
+          "type": "integer"
+      },
+      "value": {
+          "description": "Stake Value",
+          "type": "number",
+          "minimum": 0
+      }
+  },
+  "required": ["txid", "time", "value"]
+}
+EOF
+}
+
+resource "aws_api_gateway_request_validator" "verus_api_post_validate_body" {
+  name                  = "POST-body-validator"
+  rest_api_id           = aws_api_gateway_rest_api.verus_api.id
+  validate_request_body = true
+}
+
+resource "aws_api_gateway_method" "verus_api_post" {
+  authorization = "NONE"
+  //  authorization = "COGNITO_USER_POOLS"
+  //  authorizer_id        = aws_api_gateway_authorizer.verus_auth.id
+  http_method = "POST"
+  resource_id = aws_api_gateway_resource.verus_api.id
+  rest_api_id = aws_api_gateway_rest_api.verus_api.id
+  request_models = {
+    "application/json" = aws_api_gateway_model.verus_api_post_model.name
+  }
+  request_validator_id = aws_api_gateway_request_validator.verus_api_post_validate_body.id
+  //  authorization_scopes = aws_cognito_resource_server.verus_cognito_resource_server.scope_identifiers
+}
+
+resource "aws_api_gateway_integration" "verus_api_post" {
+  http_method             = aws_api_gateway_method.verus_api_post.http_method
+  resource_id             = aws_api_gateway_resource.verus_api.id
+  rest_api_id             = aws_api_gateway_rest_api.verus_api.id
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.verus_lambda.invoke_arn
+  connection_type         = "INTERNET"
+  passthrough_behavior    = "NEVER"
+  request_templates = {
+    "application/json" = <<EOF
+{
+    "body": $input.json('$'),
+    "http_method": "$context.httpMethod"
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "verus_api_method_response_post_200" {
+  http_method = aws_api_gateway_method.verus_api_post.http_method
+  resource_id = aws_api_gateway_resource.verus_api.id
+  rest_api_id = aws_api_gateway_rest_api.verus_api.id
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "verus_api_integration_response_post_200" {
+  http_method       = aws_api_gateway_method.verus_api_post.http_method
+  resource_id       = aws_api_gateway_resource.verus_api.id
+  rest_api_id       = aws_api_gateway_rest_api.verus_api.id
+  status_code       = aws_api_gateway_method_response.verus_api_method_response_post_200.status_code
+  selection_pattern = ""
+  content_handling  = "CONVERT_TO_TEXT"
+  depends_on        = [aws_api_gateway_integration.verus_api_post]
 }
 
 resource "aws_api_gateway_deployment" "verus_api" {
@@ -182,8 +283,10 @@ resource "aws_api_gateway_deployment" "verus_api" {
   triggers = {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.verus_api.id,
-      aws_api_gateway_method.verus_api.id,
-      aws_api_gateway_integration.verus_api.id,
+      aws_api_gateway_method.verus_api_get.id,
+      aws_api_gateway_integration.verus_api_get.id,
+      aws_api_gateway_method.verus_api_post.id,
+      aws_api_gateway_integration.verus_api_post.id
     ]))
   }
   lifecycle {
@@ -257,12 +360,12 @@ data "aws_iam_policy_document" "verus_assume_role_policy" {
 
 data "aws_iam_policy_document" "verus_role_inline_policy_dynamodb" {
   statement {
-    sid       = "put-item-to-verus_stakes_txids_table"
+    sid       = "PutItemToVerusStakesTxidsTable"
     actions   = ["dynamodb:PutItem"]
     resources = [aws_dynamodb_table.verus_stakes_txids_table.arn]
   }
   statement {
-    sid       = "put-get-update-item-to-verus_stakes_values_table"
+    sid       = "PutGetUpdateItemIoVerusStakesValuesTable"
     actions   = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:UpdateItem"]
     resources = [aws_dynamodb_table.verus_stakes_values_table.arn]
   }
